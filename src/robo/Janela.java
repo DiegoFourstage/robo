f598b831a49e8843288c34a1957529ff9d740382
DiegoFourstage <diego.fourstage.github@gmail.com>
2020-09-27 16:45:09 -0300

    Setando o tamanho da tela

--- src/robo/Janela.java
+++ src/robo/Janela.java
@@ -1,9 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package robo;
+
+import javax.swing.JFrame;
 
 /**
  *
@@ -11,11 +8,14 @@ package robo;
  */
 public class Janela {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        JFrame janela = new JFrame("Mr.Robot");
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        janela.setSize(1280, 720);
+        janela.setLocationRelativeTo(null);
+        janela.setResizable(false);
+
+        janela.setVisible(true);
     }
-    
+
 }
